d0fe706ba3be1ca1cda11c9cfcdb900bb2619cdc
maumlima <mauriciodemouralima@gmail.com>
2023-08-02 10:53:05 -0700

    add columns of graph model

--- src/engineering/merge_era5_grdc.jl
+++ src/engineering/merge_era5_grdc.jl
@@ -4,8 +4,35 @@ using NCDatasets
 using JSON
 using ProgressMeter
 
+function is_valid(basin_id::AbstractString, graph_dict::Dict, basin_gauge_dict::Dict)
+    if !haskey(basin_gauge_dict, basin_id)
+        return false
+    elseif !isempty(graph_dict[basin_id])
+        for up_basin in graph_dict[basin_id]
+            if !haskey(basin_gauge_dict, string(up_basin))
+                return false
+            end
+        end
+    end
+    return true
+end
+
+function get_streamflow(basin_id::AbstractString, basin_gauge_dict::Dict, grdc_ds::NCDataset, gauge_ids::Vector{Int32})::Vector{<:Real}
+    # Get gauge id to the corresponding basin
+    gauge_id = basin_gauge_dict[basin_id][1]
+
+    # Find its index in dataset
+    gauge_idx = findfirst(id -> id == gauge_id, gauge_ids)
+
+    # Extract the streamflow variable for the gauge id
+    streamflows = grdc_ds["streamflow"][gauge_idx, :]
+
+    # Poorly handle missing values
+    return replace(streamflows, missing => NaN)
+end
+
 """
-    merge_era5_grdc(timeseries_dir, grdc_nc_file, basin_gauge_dict_file, output_dir)
+    merge_era5_grdc(timeseries_dir, grdc_nc_file, basin_gauge_dict_file, graph_dict_file, shape_file, output_dir)
 
 Merges ERA5 timeseries data for each basin defined by HydroSHEDS with the corresponding GRDC gauge streamflow series.
 
@@ -13,6 +40,8 @@ Merges ERA5 timeseries data for each basin defined by HydroSHEDS with the corres
 - `timeseries_dir::String`: directory path containing the ERA5 timeseries data files for each basin.
 - `grdc_nc_file::String`: path to the NetCDF file containing the GRDC gauge data.
 - `basin_gauge_dict_file::String`: path to the JSON file containing the mapping between basin IDs and corresponding GRDC gauge IDs.
+- `graph_dict_file::String`: path to the JSON file containing the graph dictionary.
+- `shape_file:::String`: path to the HydroSHEDS shapefile.
 - `output_dir::String`: directory path where the merged data files will be saved.
 
 # Output
@@ -20,7 +49,12 @@ Merges ERA5 timeseries data for each basin defined by HydroSHEDS with the corres
 - Recomended for the `output_dir` to be of the kind **"path/to/timeseries/timeseries_lvXX"** for a good communication with the model
 (where XX is the level in HydroSHEDS).
 """
-function merge_era5_grdc(timeseries_dir::String, grdc_nc_file::String, basin_gauge_dict_file::String, output_dir::String)
+function merge_era5_grdc(timeseries_dir::String, 
+                         grdc_nc_file::String, 
+                         basin_gauge_dict_file::String, 
+                         graph_dict_file::String,
+                         shape_file,
+                         output_dir::String)
     # Get a list of all files in the timeseries directory
     basin_files = readdir(timeseries_dir)
 
@@ -32,6 +66,9 @@ function merge_era5_grdc(timeseries_dir::String, grdc_nc_file::String, basin_gau
     # Read matching dictionary
     basin_gauge_dict = JSON.parsefile(basin_gauge_dict_file)
 
+    # Read graph
+    graph_dict = JSON.parsefile(graph_dict_file)
+
     # Get min and max date of the basin time series
     basin_df = CSV.read(joinpath(timeseries_dir, basin_files[1]), DataFrame)
     min_date, max_date = basin_df.date[1], basin_df.date[end]
@@ -39,6 +76,9 @@ function merge_era5_grdc(timeseries_dir::String, grdc_nc_file::String, basin_gau
     # Find NetCDF corresponding index
     min_date_idx = findfirst(date -> date == min_date, dates)
     max_date_idx = findfirst(date -> date == max_date, dates)
+
+    # Open the shapefile
+    shape_df = Shapefile.Table(shape_file) |> DataFrame
 
     # Create output directory
     mkpath(output_dir)
@@ -49,26 +89,37 @@ function merge_era5_grdc(timeseries_dir::String, grdc_nc_file::String, basin_gau
         basin_df = CSV.read(joinpath(timeseries_dir, basin_file), DataFrame)
     
         # Check if the basin id exists in the basin_gauge_dictionary
-        if haskey(basin_gauge_dict, basin_id)
-            # Get gauge id to the corresponding basin
-            gauge_id = basin_gauge_dict[basin_id][1]
-            
-            # Find its index in dataset
-            gauge_idx = findfirst(id -> id == gauge_id, gauge_ids)
-            
-            # Extract the streamflow variable for the gauge id
-            streamflows = grdc_ds["streamflow"][gauge_idx, :]
-            
-            # Poorly handle missing values
-            streamflows_with_NaN = replace(streamflows, missing => NaN)
+        if is_valid(basin_id, graph_dict, basin_gauge_dict)
+            # Get streamflow timeseries
+            streamflow = get_streamflow(basin_id, basin_gauge_dict, grdc_ds, gauge_ids)
             
             # Add the streamflow values to the basin data
-            basin_df[!, "streamflow"] = streamflows_with_NaN[min_date_idx:max_date_idx]
+            basin_df[!, "streamflow"] = streamflow[min_date_idx:max_date_idx]
+
+            # Add upstream (sum and pond)
+            down_dist = shape_df[shape_df.HYBAS_ID .== parse(Int, basin_id), :DIST_MAIN][1]
+            pond_upstream = zeros(max_date_idx-min_date_idx+1)
+            upstream = zeros(max_date_idx-min_date_idx+1)
+            dist_sum = 0
+            if !isempty(graph_dict[basin_id])
+                for up_basin in graph_dict[basin_id]
+                    up_dist = shape_df[shape_df.HYBAS_ID .== up_basin, :DIST_MAIN][1]
+                    dist = down_dist - up_dist
+                    streamflow = get_streamflow(string(up_basin), basin_gauge_dict, grdc_ds, gauge_ids)[min_date_idx:max_date_idx]
+                    upstream += streamflow
+                    pond_upstream += streamflow * dist
+                    dist_sum += dist
+                end
+                pond_upstream = pond_upstream / dist_sum
+            end
+            basin_df[!, "upstream"] = upstream
+            basin_df[!, "pond_upstream"] = pond_upstream
 
             # Write the merged data to a new file
             CSV.write(joinpath(output_dir, basin_file), basin_df)
         end
     end
+
     # Close the NetCDF file
     close(grdc_ds)
 end
