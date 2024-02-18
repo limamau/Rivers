d8f78162c25a790502806389f2bb4feed2a713da
maumlima <mauriciodemouralima@gmail.com>
2024-02-18 03:57:51 -0800

    change variables2 -> variables

--- examples/engineering/download_data_scripts/api_land_retrieval.py
+++ examples/engineering/download_data_scripts/api_land_retrieval.py
@@ -20,7 +20,7 @@ for year in range(year_start, year_end + 1):
             "reanalysis-era5-land",
             {
                 "product_type": product_type,
-                "variable": variables2,
+                "variable": variables,
                 "year": [str(year)],
                 "month": [str(month).zfill(2)],
                 "day": [str(day).zfill(2) for day in range(day_start, day_end + 1)],
@@ -28,5 +28,5 @@ for year in range(year_start, year_end + 1):
                 "grid":['0.1','0.1'],
                 "format": "netcdf",
             },
-            "evaporation_year_month/era5_" + str(year) + "_" + str(month).zfill(2) + ".nc",
+            "globe_year_month/era5_" + str(year) + "_" + str(month).zfill(2) + ".nc",
         )
