63df7f7f2ceca0681eec1a1ae1bfefabb3c79638
Andrew J. Chiang <achiang@hpc-25-09.cm.cluster>
2024-04-29 00:02:36 -0700

    create plots for NSE vs epoch for all runs

--- examples/catchment_models/training/script.sh
+++ examples/catchment_models/training/script.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-#SBATCH --job-name=globe_basin_split
-#SBATCH --output=/home/achiang/CliMA/Rivers/training_out_files/%j.out
-#SBATCH --error=/home/achiang/CliMA/Rivers/training_err_files/%j.err
+#SBATCH --job-name=globe_time_eval
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --gres=gpu:v100:1 # remove :v100 for p100    
-#SBATCH --time=9:00:00 # adjust as necessary
+#SBATCH --time=1:00:00 # adjust as necessary
 #SBATCH --mem-per-gpu=16GB # adjust as necessary
 #SBATCH --mail-user=achiang@caltech.edu
+#SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
 set -euo pipefail # kill the job if anything fails
 set -x # echo script
-nh-run train --config-file /home/achiang/CliMA/Rivers/examples/catchment_models/training/globe_basin_split.yml #nh-run ...?
+nh-run evaluate --run-dir /home/achiang/CliMA/runs/globe_time_split_2404_124848/ #nh-run ...?
 echo done
