ba2cfaf9a300bb7b04985ab46d6f40a2c8e1f1d0
Andrew J. Chiang <achiang@hpc-21-18.cm.cluster>
2024-07-25 12:26:07 -0700

    Create continue from epoch script

---
+++ examples/catchment_models/neuralhydrology/nh_continue.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=usa_time_split_nse_adj3_2407_195234
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --gres=gpu:1 # :v100:1 or p100:1  
+#SBATCH --time=10:00:00 # adjust as necessary
+#SBATCH --mem-per-gpu=16GB # adjust as necessary
+#SBATCH --mail-user=achiang@caltech.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+set -euo pipefail # kill the job if anything fails
+set -x # echo script
+nh-run continue_training --run-dir /home/achiang/CliMA/Rivers/examples/catchment_models/neuralhydrology/runs/usa_time_split_nse_adj3_2407_195234
+echo done
