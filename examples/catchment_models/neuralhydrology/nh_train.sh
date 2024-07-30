2b62a34e5feb36fee349633651082acd0ca3a239
Andrew J. Chiang <achiang@hpc-22-34.cm.cluster>
2024-07-29 17:49:50 -0700

    histogram of streamflows

--- examples/catchment_models/neuralhydrology/nh_train.sh
+++ examples/catchment_models/neuralhydrology/nh_train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=usa_time_nse_lr9.yml
+#SBATCH --job-name=usa_time_512nhid_35epochs.yml
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --gres=gpu:1 # :v100:1 or p100:1  
-#SBATCH --time=18:00:00 # adjust as necessary
+#SBATCH --time=01:00:00 # adjust as necessary
 #SBATCH --mem-per-gpu=16GB # adjust as necessary
 #SBATCH --mail-user=achiang@caltech.edu
 #SBATCH --mail-type=BEGIN
@@ -14,5 +14,5 @@
 
 set -euo pipefail # kill the job if anything fails
 set -x # echo script
-nh-run train --config-file /groups/esm/achiang/Rivers/examples/catchment_models/neuralhydrology/usa_time_nse_lr9.yml
+nh-run train --config-file /groups/esm/achiang/Rivers/examples/catchment_models/neuralhydrology/usa_time_512nhid_35epochs.yml
 echo done
