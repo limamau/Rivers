4589fa1716e1ac92d72ad36718d3a6e0556fe2bf
Andrew J. Chiang <achiang@hpc-23-32.cm.cluster>
2024-08-06 11:12:39 -0700

    histograms of adataptive dt

--- examples/catchment_models/neuralhydrology/get_weights.py
+++ examples/catchment_models/neuralhydrology/get_weights.py
@@ -11,7 +11,9 @@ from config import Config
 
 from pathlib import Path
 
-run_dir = 'usa_time_split_nseMse_adaDT1_0108_121334'
+run_dir = 'usa_time_split_nse_adaDT5_0508_103715'
+split = run_dir.split('_')
+test_name = f'{split[3]} {split[4]}'
 run_path = Path(f'/groups/esm/achiang/Rivers/examples/catchment_models/neuralhydrology/runs/{run_dir}/config.yml')
 cfg = Config(run_path)
 
@@ -20,31 +22,26 @@ model = coRNN(cfg=cfg)
 
 model.load_state_dict(torch.load(weight_file))
 
+def create_histogram(tensor, name, edges):
+    data = tensor.numpy()
+
+    plt.figure()
+    plt.hist(data, bins=edges, edgecolor='black')
+    plt.title(f"{test_name}: {name}")
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.savefig(f"histograms/{name}.png")
+    plt.close()
+
 for param_tensor in model.state_dict():
 
     # print(f"{param_tensor}: {model.state_dict()[param_tensor].size()}")
     if param_tensor == 'cell.c':
         c = model.state_dict()[param_tensor]
-        # hist, bin_edges = np.histogram(c, bins=25)
+        c_edges = np.arange(-6, 6.01, 0.2)
+        create_histogram(c, f"c_hist_{split[3]}_{split[4]}", c_edges)
 
-        # plt.figure(1)
-        # plt.hist(hist, bins=bin_edges, edgecolor='black')
-        # plt.title('Histogram of c')
-        # plt.xlabel('c')
-        # plt.ylabel('Frequency')
-        # plt.savefig("c_histogram.png")
-        # plt.close()
-
-        print(param_tensor, "\t", c)
-        sigma_hat = 1/12 + 1/12 * torch.tanh(c / 2)
-        hist, bin_edges = torch.histogram(sigma_hat, bins = 25)
-        print(f'sigma_hat : {sigma_hat}')
-        # hist, bin_edges2 = np.histogram(sigma_hat, bins=2)
-
-        plt.figure()
-        plt.hist(hist, bins=bin_edges, edgecolor='black')
-        plt.title('Histogram of sigma')
-        plt.xlabel('sigma')
-        plt.ylabel('Frequency')
-        plt.savefig("sigma_histogram.png")
-        plt.close()
+        dt_bound = 1/5
+        sigma_hat = (dt_bound/2 + dt_bound/2 * torch.tanh(c / 2))
+        sigma_edges = np.arange(0,dt_bound+0.01, 0.01)
+        create_histogram(sigma_hat, f"sigma_hist_{split[3]}_{split[4]}", sigma_edges)
